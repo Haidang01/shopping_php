8338e06d0f749a22ba2bef94faab19c6e531fdf3
LuuThiHuyenTrang <cupitk3102@gmail.com>
2023-02-16 22:14:35 +0700

    upload_thong_thongke

--- controllers/deleteUser.php
+++ controllers/deleteUser.php
@@ -2,6 +2,15 @@
 try {
     include '../model/connectDb.php';
     $id = $_GET['id'];
+
+    $q2 = "DELETE FROM gio_hang WHERE `gio_hang`.`id_user` = $id";
+    $statement = $connect->prepare($q2);
+    $result1 = $statement->execute();
+
+    $q3 = "DELETE FROM binh_luan WHERE `binh_luan`.`id_user` = $id";
+    $statement = $connect->prepare($q3);
+    $result3 = $statement->execute();
+
     $q = "DELETE FROM nguoi_dung WHERE `nguoi_dung`.`id` = $id";
     $statement = $connect->prepare($q);
     $result = $statement->execute();
