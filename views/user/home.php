1eecf14afab7513e5326f0e3ec9af2f93f8e83bf
Haidang01 <vuhonghaidang15@gmail.com>
2023-02-28 21:03:57 +0700

    afs

--- views/user/home.php
+++ views/user/home.php
@@ -35,7 +35,9 @@ if ($params == 'all' && !isset($_POST['name'])) {
     <div class="boxleft mr ">
       <div class="row">
         <div class="banner">
-          <img src="https://thietkehaithanh.com/wp-content/uploads/2019/06/huong-dan-thiet-ke-banner-dien-thoai-bang-photoshop.jpg" alt="">
+          <img
+            src="https://thietkehaithanh.com/wp-content/uploads/2019/06/huong-dan-thiet-ke-banner-dien-thoai-bang-photoshop.jpg"
+            alt="">
         </div>
         <div class="row">
           <h2 class="h2title">SẢN PHẨM </h2>
@@ -43,14 +45,15 @@ if ($params == 'all' && !isset($_POST['name'])) {
             echo '<h2 class="mt-5">Không có sản phẩm phù hợp</h2>';
           } else {
           ?>
-            <?php foreach ($products as $top) { ?>
-              <div class="boxspp mrs">
-                <div class=" row img">
-                  <a href="./chitiet.php?id=<?= $top['id'] ?>"><img src='../../controllers/<?php echo $top['anh']; ?>' style="height:200px; object-fit:cover;" alt=""></a>
-                </div>
-                <p class="price"><?php echo $top['price']; ?></p>
-                <span class="prices"><?php echo $top['name']; ?></span>
-              </div>
+          <?php foreach ($products as $top) { ?>
+          <div class="boxspp mrs">
+            <div class=" row img">
+              <a href="./chitiet.php?id=<?= $top['id'] ?>"><img src='../../controllers/<?php echo $top['anh']; ?>'
+                  style="height:200px; object-fit:cover;" alt=""></a>
+            </div>
+            <p class="price"><?php echo $top['price']; ?></p>
+            <span class="prices"><?php echo $top['name']; ?></span>
+          </div>
           <?php }
           } ?>
         </div>
@@ -61,54 +64,55 @@ if ($params == 'all' && !isset($_POST['name'])) {
         <div class="boxtitle">TÀI KHOẢN</div>
         <div class="boxcontent formtaikhoan">
           <?php if (isset($_SESSION['userId'])) { ?>
-            <div class="row mb10">
-              <img src="/shopping_php/controllers/<?= $_SESSION['img'] ?>" alt="avata" style="width: 45px !important; height: 45px; border-radius: 50%;" class='rounded-circle'>
-              <h5>Xin chào, <?= $_SESSION['username'] ?></h5>
-            </div>
-            <div class="row mb10">
-              <li>
-                <a href="/shopping_php/views/user/changePassword.php">Quên mật khẩu</a>
-              </li>
-              <li>
-                <a href="/shopping_php/views/user/changeInfo.php">Cập nhật tài khoản</a>
-              </li>
-              <?php if ($_SESSION['role'] == 1) { ?>
-                <li>
-                  <a href="/shopping_php/views/admin/quanliuser.php">Đăng nhập Admin</a>
-                </li>
-              <?php } ?>
-              <li>
-                <a href="/shopping_php/controllers/logout.php">Đăng xuất</a>
-              </li>
-            </div>
+          <div class=" mb10 d-flex align-items-center gap-3">
+            <img src="/shopping_php/controllers/<?= $_SESSION['img'] ?>" alt="avata"
+              style="width: 45px !important; height: 45px; border-radius: 50%;" class='rounded-circle'>
+            <h5 class='fs-5 text-primary fw-bold'>Xin chào, <?= $_SESSION['username'] ?></h5>
+          </div>
+          <div class="row mb10">
+            <li>
+              <a href="/shopping_php/views/user/changePassword.php">Quên mật khẩu</a>
+            </li>
+            <li>
+              <a href="/shopping_php/views/user/changeInfo.php">Cập nhật tài khoản</a>
+            </li>
+            <?php if ($_SESSION['role'] == 1) { ?>
+            <li>
+              <a href="/shopping_php/views/admin/quanliuser.php">Đăng nhập Admin</a>
+            </li>
+            <?php } ?>
+            <li>
+              <a href="/shopping_php/controllers/logout.php">Đăng xuất</a>
+            </li>
+          </div>
           <?php
           } else {
           ?>
-            <form action="/shopping_php/controllers/loginUser.php" method="post" style="width: 110%;">
-              <div class="row mb10">
-                Email đăng nhập <br>
-                <input type="email" name="email">
-              </div>
-              <div class="row mb10">
-                Mật khẩu <br>
-                <input type="password" name="pass">
-              </div>
-              <div class="row mb10" style="display: flex;">
-                <div><input type="checkbox" name=""></div>
-                <div>Ghi nhớ tài khoản?</div>
-              </div>
-              <div class="row mb10">
-                <input type="submit" value="Đăng nhập" name="dangnhap">
-              </div>
-            </form>
-            <div>
-              <li>
-                <a href="#">Quên mật khẩu</a>
-              </li>
-              <li>
-                <a href="index.php?act=dangky">Đăng ký thành viên</a>
-              </li>
+          <form action="/shopping_php/controllers/loginUser.php" method="post" style="width: 110%;">
+            <div class="row mb10">
+              Email đăng nhập <br>
+              <input type="email" name="email">
             </div>
+            <div class="row mb10">
+              Mật khẩu <br>
+              <input type="password" name="pass">
+            </div>
+            <div class="row mb10" style="display: flex;">
+              <div><input type="checkbox" name=""></div>
+              <div>Ghi nhớ tài khoản?</div>
+            </div>
+            <div class="row mb10">
+              <input type="submit" value="Đăng nhập" name="dangnhap">
+            </div>
+          </form>
+          <div>
+            <li>
+              <a href="#">Quên mật khẩu</a>
+            </li>
+            <li>
+              <a href="index.php?act=dangky">Đăng ký thành viên</a>
+            </li>
+          </div>
           <?php
           } ?>
         </div>
